5e53f19f7a6b6be41b65c37e3f040b855a20a4ab
sudha <Sudhaas2802@gmail.com>
2025-12-09 13:55:54 -0500

    expense

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -5,12 +5,7 @@ terraform {
       version = "6.0.0-beta3"
     }
   }
-  backend "s3" {
-    bucket = "terraform-tfstate-dev1"
-    key    = "expense-dev-vpc1"
-    region = "us-east-1"
-    dynamodb_table = "terraform-dev-locking"
-  }
+  
 }
 
 
