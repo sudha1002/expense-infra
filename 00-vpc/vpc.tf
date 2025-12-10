bcad267124b4fd5415911c76f7c2b188d440a8fb
sudha <Sudhaas2802@gmail.com>
2025-12-10 16:51:27 -0500

    expense

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -1,16 +1,18 @@
 module "vpc" {
+    #source = "../terraform-aws-vpc"
     source = "git::https://github.com/sudha1002/terraform-aws-vpc.git?ref=main"
-    vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
-    vpc_tags = var.vpc_tags
+    vpc_cidr = var.vpc_cidr
     common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
     public_subnet_cidrs = var.public_subnet_cidrs
     private_subnet_cidrs = var.private_subnet_cidrs
     database_subnet_cidrs = var.database_subnet_cidrs
     is_peering_required = true
-
 }
+
+# this can be included in module
 resource "aws_db_subnet_group" "expense" {
   name       = "${var.project_name}-${var.environment}"
   subnet_ids = module.vpc.database_subnet_ids
@@ -18,7 +20,7 @@ resource "aws_db_subnet_group" "expense" {
   tags = merge(
     var.common_tags,
     {
-        Name = "${var.project_name}-${var.environment}-new"
+        Name = "${var.project_name}-${var.environment}"
     }
   )
 }
