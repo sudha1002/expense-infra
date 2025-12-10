6ec61c577aba54d23cc7aa2bc7d4bb30b83ac320
sudha <Sudhaas2802@gmail.com>
2025-12-10 16:52:11 -0500

    expense

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -19,7 +19,7 @@ resource "aws_ssm_parameter" "database_subnet_ids" {
   value = join(",", module.vpc.database_subnet_ids)
 } 
 resource "aws_ssm_parameter" "database_subnet_group_name" {
-  name  = "/${var.project_name}/${var.environment}-new/database_subnet_group_name"
+  name  = "/${var.project_name}/${var.environment}/database_subnet_group_name"
   type  = "String"
   value = aws_db_subnet_group.expense.name
 } 
